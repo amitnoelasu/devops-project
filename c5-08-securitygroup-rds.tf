0d5de0d3b9fb33fdfbcabc64bb1ef8d8b8a3696b
AMIT NOEL <athokal1@asu.edu>
2026-02-07 02:07:30 -0600

    added RDS (MySQL), deployed spring app

--- c5-08-securitygroup-rds.tf
+++ c5-08-securitygroup-rds.tf
@@ -1,15 +1,25 @@
-module "private-sg" {
+module "rdsdb-sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.3.1"
 
-  name = "private-sg"
+  name = "rdsdb-sg"
   description = "security group with SSH and HTTP port open for everybody (ipv4 CIDR), egress ports are all world open"
 
   vpc_id = module.vpc.vpc_id
 
   # ingress rules
-  ingress_cidr_blocks = [module.vpc.vpc_cidr_block]
-  ingress_rules       = ["http-80-tcp", "ssh-tcp"]
+
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 3306
+      to_port     = 3306
+      protocol    = "tcp"
+      description = "MySQL access from private subnets"
+      cidr_blocks = join(",", module.vpc.private_subnets_cidr_blocks)
+    }
+  ]
+
+
   # egress rules
   egress_rules = ["all-all"]
   tags = local.common_tags
